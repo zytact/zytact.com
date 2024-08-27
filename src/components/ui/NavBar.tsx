be7ba5674e1d0d674a1502bd681fcc09731e0a74
Arnab Chakraborty <iam-arnab@outlook.com>
2024-08-27 08:22:45 +0530

    adding gradient

--- src/components/ui/NavBar.tsx
+++ src/components/ui/NavBar.tsx
@@ -2,7 +2,9 @@ import Link from 'next/link';
 
 export default function NavBar({ className }: { className?: string }) {
     return (
-        <nav className={`${className} flex gap-10 font-medium`}>
+        <nav
+            className={`${className} flex gap-12 bg-gradient-to-b from-neutral-50 to-neutral-400 bg-clip-text font-medium text-transparent`}
+        >
             <Link href="/">Home</Link>
             <Link href="/skills">Skills</Link>
             <Link href="/projects">Projects</Link>
