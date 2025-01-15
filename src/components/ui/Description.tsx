58d76450dab0639870c5c58f338dcf845a6e9707
Arnab Chakraborty <iam-arnab@outlook.com>
2025-01-15 17:29:15 +0530

    increase age

--- src/components/ui/Description.tsx
+++ src/components/ui/Description.tsx
@@ -1,7 +1,7 @@
 'use client';
 import { TextGenerateEffect } from './text-generate-effect';
 
-const words = `I am a 20 y/o IT Undergrad Student. I am learning Fullstack Web Development. When I am not coding, I am probably watching anime, obsessing over philosophy and mythology or doing something stupid.`;
+const words = `I am a 21 y/o IT Undergrad Student. I am learning Fullstack Web Development. When I am not coding, I am probably watching anime, obsessing over philosophy and mythology or doing something stupid.`;
 
 export function Description() {
     return <TextGenerateEffect duration={2} filter={false} words={words} />;
