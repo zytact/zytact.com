95720cb8f8d0c861bfa3013e087cc4267aec2255
Arnab Chakraborty <iam-arnab@outlook.com>
2025-01-19 21:54:32 +0530

    move prettier config from json to es module js

---
+++ prettier.config.mjs
@@ -0,0 +1,13 @@
+/**
+ * @type {import("prettier").Config}
+ */
+
+const config = {
+    trailingComma: "es5",
+    tabWidth: 4,
+    semi: true,
+    singleQuote: true,
+    plugins: ["prettier-plugin-tailwindcss"]
+}
+
+export default config;
