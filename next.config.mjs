59ec5b350902617bde49806e93d31f175dbedf9d
Arnab Chakraborty <iam-arnab@outlook.com>
2025-01-27 17:40:03 +0530

    remove million lint

--- next.config.mjs
+++ next.config.mjs
@@ -2,19 +2,19 @@
 const nextConfig = {
     images: {
         remotePatterns: [
-      {
-        protocol: 'https',
-        hostname: 'utfs.io',
-        port: '',
-        pathname: '/f/**',
-      },
-      {
-        protocol: 'https',
-        hostname: 'api.microlink.io',
-        port: '',
-        pathname: '/**',
-      },
-    ],
+            {
+                protocol: 'https',
+                hostname: 'utfs.io',
+                port: '',
+                pathname: '/f/**',
+            },
+            {
+                protocol: 'https',
+                hostname: 'api.microlink.io',
+                port: '',
+                pathname: '/**',
+            },
+        ],
     },
 };
 
