18c292106c0465bac4e7a2950b78d1ad11d83093
Arnab Chakraborty <iam-arnab@outlook.com>
2025-02-23 22:55:18 +0530

    fix

--- src/app/robots.ts
+++ src/app/robots.ts
@@ -6,6 +6,6 @@ export default function robots(): MetadataRoute.Robots {
             userAgent: '*',
             allow: '/',
         },
-        sitemap: `${process.env.BASE_URL}/sitemap.xml`,
+        sitemap: `${process.env.NEXT_PUBLIC_BASE_URL}/sitemap.xml`,
     };
 }
