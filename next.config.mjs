1b3fabf1e482c2672130e52ae3d7353bd0fec158
Arnab Chakraborty <iam-arnab@outlook.com>
2025-03-02 01:20:31 +0530

    setup 301 redirect

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+
     images: {
         remotePatterns: [
             {
@@ -15,6 +16,32 @@ const nextConfig = {
                 pathname: '/**',
             },
         ],
+    },
+    async redirects() {
+        return [
+            {
+                source: '/:path*',
+                has: [
+                    {
+                        type: 'host',
+                        value: 'zytact.in',
+                    },
+                ],
+                destination: 'https://zytact.com/:path*',
+                permanent: true, // This sets up a 301 redirect
+            },
+            {
+                source: '/:path*',
+                has: [
+                    {
+                        type: 'host',
+                        value: 'www.zytact.in',
+                    },
+                ],
+                destination: 'https://www.zytact.com/:path*',
+                permanent: true,
+            },
+        ];
     },
 };
 
