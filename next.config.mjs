34ae4bdb061de65bc09b05c229eaa96e5dbe67f6
Arnab Chakraborty <iam-arnab@outlook.com>
2025-04-17 23:22:13 +0530

    remove unused image remote patterns from next config

--- next.config.mjs
+++ next.config.mjs
@@ -1,22 +1,5 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-
-    images: {
-        remotePatterns: [
-            {
-                protocol: 'https',
-                hostname: 'utfs.io',
-                port: '',
-                pathname: '/f/**',
-            },
-            {
-                protocol: 'https',
-                hostname: 'api.microlink.io',
-                port: '',
-                pathname: '/**',
-            },
-        ],
-    },
     async redirects() {
         return [
             {
