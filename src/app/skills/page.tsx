10140026e6a12c8f9fedd674ed5dbabb67090b45
Arnab Chakraborty <iam-arnab@outlook.com>
2025-04-20 13:15:55 +0530

    reduce description for better auth

--- src/app/skills/page.tsx
+++ src/app/skills/page.tsx
@@ -5074,7 +5074,7 @@ const grid: Skill[] = [
         id: 12,
         title: 'Better Auth',
         description:
-            'Better Auth is a framework-agnostic authentication and authorization framework for TypeScript. It provides a comprehensive set of features out of the box and includes a plugin ecosystem that simplifies adding advanced functionalities. Whether you need 2FA, multi-tenancy, multi-session support, or even enterprise features like SSO, it lets you focus on building your application instead of reinventing the wheel.',
+            'Better Auth is a framework-agnostic authentication and authorization framework for TypeScript. It provides a comprehensive set of features out of the box and includes a plugin ecosystem that simplifies adding advanced functionalities.',
         link: 'https://www.better-auth.com/',
     },
     {
