b42c5e2c22229174799f9ac21cdcc79f9b1a2bd2
Arnab Chakraborty <iam-arnab@outlook.com>
2025-04-23 01:15:21 +0530

    set up vercel analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from 'next';
 import { Inter } from 'next/font/google';
+import { Analytics } from '@vercel/analytics/react';
 import './globals.css';
 
 const inter = Inter({ subsets: ['latin'] });
@@ -20,6 +21,7 @@ export default function RootLayout({
 }>) {
     return (
         <html lang="en">
+            <Analytics />
             <body className={inter.className}>{children}</body>
         </html>
     );
